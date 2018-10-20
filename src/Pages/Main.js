f51412d8fa62faa4a7ba3f0a5adede5a8126959b
Davis Jordan <jorda128@purdue.edu>
2018-10-20 02:27:49 -0400

    main page ui

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
+import './CSS/Main.css';
+
 class Main extends Component {
+  constructor() {
+      super();
+      this.login = this.login.bind(this);
+  }
+
+  login() {
+    console.log("button pressed");
+  }
+
   render() {
     return (
       <div>
-        This is the main page
+        <h1 style={{color: '#ff9507'}} className='Title'>
+          Mergify
+        </h1>
+        <body style={{color: 'white'}} className='text'>
+          Welcome!<br />
+          To get started, click <br />
+          the button below to sign in <br />
+          to Spotify!
+          <br /><br /><br /><br />
+        </body>
+        <div className='buttonArea'>
+          <button className='button' style={{background:'#ff9507'}} onClick={this.login}>
+            Login With Spotify
+          </button>
+        </div>
       </div>
     );
   }
