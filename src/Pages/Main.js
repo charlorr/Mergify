9185aaa6fb6373b6647e5d624f1f5b0b188b96b2
Davis Jordan <jorda128@purdue.edu>
2018-10-20 01:35:13 -0400

    add main and 404 pages

---
+++ src/Pages/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        This is the main page
+      </div>
+    );
+  }
+}
+
+export default Main;
